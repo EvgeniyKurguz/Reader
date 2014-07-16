c4c0cb9dc97b07863cff4cceb138129a1e3a5446
EvgeniyKurguz <steppe90@mail.ru>
2014-07-16 14:29:56 +0600

    Reader

--- src/main/java/com/epam/Runner.java
+++ src/main/java/com/epam/Runner.java
@@ -37,7 +37,7 @@ public class Runner {
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         reading();
         writing();
 
