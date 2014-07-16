4b5748eab8cc456c7eacce60cf9efd2de8991d4b
Администратор <steppe90@mail.ru>
2014-07-16 12:18:05 +0600

    file

---
+++ src/main/java/com/epam/Runner.java
@@ -0,0 +1,46 @@
+package com.epam;
+
+import java.io.*;
+
+public class Runner {
+    private static StringBuilder str = new StringBuilder();
+
+    private static void reading() throws IOException {
+        File in = new File("input.txt");
+        try {
+            FileReader reader = new FileReader(in);
+            int n;
+            while ((n = reader.read()) != -1) {
+                char c = (char) n;
+                if (c != 'a' && c != 'e' && c != 'y') {
+                    str.append(c);
+                }
+            }
+            reader.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static void writing() throws IOException {
+        File out = new File("output.txt");
+        try {
+
+            FileWriter writer = new FileWriter(out);
+            writer.write(str.toString());
+            writer.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        reading();
+        writing();
+
+
+    }
+}
